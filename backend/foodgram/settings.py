580402e0d4310007de15a6db5214c8b2b7f79765
Антон Плотицын <plotitsynanton@yandex.ru>
2023-03-03 22:31:23 +0300

    workflow: deploy

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -6,7 +6,10 @@ load_dotenv()
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-SECRET_KEY = 'rv3hjm63&!x&ldxaru_td_xfee9w%t0d00(rwr9rfuwx*@1jcf'
+SECRET_KEY = os.getenv(
+    'SECRET_KEY',
+    default='rv3hjm63&!x&ldxaru_td_xfee9w%t0d00(rwr9rfuwx*@1jcf'
+)
 
 DEBUG = True
 
@@ -58,21 +61,18 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'foodgram.wsgi.application'
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-#     }
-# }
 
 DATABASES = {
     'default': {
-        'ENGINE': os.getenv('DB_ENGINE', 'django.db.backends.postgresql'),
-        'NAME': os.getenv('DB_NAME'), 
-        'USER': os.getenv('POSTGRES_USER'),
-        'PASSWORD': os.getenv('POSTGRES_PASSWORD'),
-        'HOST': os.getenv('DB_HOST'),
-        'PORT': os.getenv('DB_PORT')
+        'ENGINE': os.getenv(
+            'DB_ENGINE',
+            default='django.db.backends.postgresql'
+        ),
+        'NAME': os.getenv('DB_NAME', default='postgres'),
+        'USER': os.getenv('POSTGRES_USER', default='postgres'),
+        'PASSWORD': os.getenv('POSTGRES_PASSWORD', default='postgres'),
+        'HOST': os.getenv('DB_HOST', default='db'),
+        'PORT': os.getenv('DB_PORT', default='5432')
     }
 }
 
