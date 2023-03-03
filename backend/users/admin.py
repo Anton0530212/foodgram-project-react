5927e426c12d464310b5404bb60f391c2a11ba66
Антон Плотицын <plotitsynanton@yandex.ru>
2023-03-04 01:00:19 +0300

    README.md

--- backend/users/admin.py
+++ backend/users/admin.py
@@ -3,6 +3,8 @@ from django.contrib.auth.models import Group
 
 from .models import User, Subscription
 
+
+
 admin.site.unregister(Group)
 admin.site.register(User)
 admin.site.register(Subscription)
